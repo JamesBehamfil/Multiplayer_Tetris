1972aa2c85e7fc31112c3f0268e9f906954324eb
JamesBehamfil <congbinh230404@gmail.com>
2025-04-07 14:44:02 +0700

    Add files via upload

---
+++ game.py
@@ -0,0 +1,115 @@
+from grid import Grid
+from blocks import *
+import random
+import pygame
+
+class Game:
+	def __init__(self):
+		self.grid = Grid()
+		self.blocks = [IBlock(), JBlock(), LBlock(), OBlock(), SBlock(), TBlock(), ZBlock()]
+		self.current_block = self.get_random_block()
+		self.next_block = self.get_random_block()
+		self.game_over = False
+		self.score = 0
+		self.rotate_sound = pygame.mixer.Sound("Sounds/rotate.ogg")
+		self.clear_sound = pygame.mixer.Sound("Sounds/clear.ogg")
+
+		pygame.mixer.music.load("Sounds/music.ogg")
+		pygame.mixer.music.play(-1)
+
+	# Cập nhật điểm khi hàng đầy (1 hàng = 100, 2 hàng = 300, 3 hàng = 500, 4 hàng = 1000):
+	def update_score(self, lines_cleared, move_down_points):
+		if lines_cleared == 1:
+			self.score += 100
+		elif lines_cleared == 2:
+			self.score += 300
+		elif lines_cleared == 3:
+			self.score += 500
+		elif lines_cleared == 4:
+			self.score += 1000
+		self.score += move_down_points
+
+	# Tạo một khối ngẫu nhiên
+	def get_random_block(self):
+		if len(self.blocks) == 0:
+			self.blocks = [IBlock(), JBlock(), LBlock(), OBlock(), SBlock(), TBlock(), ZBlock()]
+		block = random.choice(self.blocks)
+		self.blocks.remove(block)
+		return block
+	
+	# Di chuyển khối sang trái
+	def move_left(self):
+		self.current_block.move(0, -1)
+		if self.block_inside() == False or self.block_fits() == False:
+			self.current_block.move(0, 1)
+
+	# Di chuyển khối sang phải
+	def move_right(self):
+		self.current_block.move(0, 1)
+		if self.block_inside() == False or self.block_fits() == False:
+			self.current_block.move(0, -1)
+
+	# Di chuyển khối xuống dưới 1 hàng
+	def move_down(self):
+		self.current_block.move(1, 0)
+		if self.block_inside() == False or self.block_fits() == False:
+			self.current_block.move(-1, 0)
+			self.lock_block()
+
+	# Khi một khối nằm ở cuối hàng hoặc ở trên 1 khối khác, khối đó sẽ không di chuyển nữa
+	def lock_block(self):
+		tiles = self.current_block.get_cell_positions()
+		for position in tiles:
+			self.grid.grid[position.row][position.column] = self.current_block.id
+		self.current_block = self.next_block
+		self.next_block = self.get_random_block()
+		rows_cleared = self.grid.clear_full_rows()
+		if rows_cleared > 0:
+			self.clear_sound.play()
+			self.update_score(rows_cleared, 0)
+		if self.block_fits() == False:
+			self.game_over = True
+
+	# Reset lại bảng game (sau khi trò chơi kết thúc)
+	def reset(self):
+		self.grid.reset()
+		self.blocks = [IBlock(), JBlock(), LBlock(), OBlock(), SBlock(), TBlock(), ZBlock()]
+		self.current_block = self.get_random_block()
+		self.next_block = self.get_random_block()
+		self.score = 0
+
+	# Xóa hàng đã đầy khối
+	def block_fits(self):
+		tiles = self.current_block.get_cell_positions()
+		for tile in tiles:
+			if self.grid.is_empty(tile.row, tile.column) == False:
+				return False
+		return True
+
+	# Xoay khối
+	def rotate(self):
+		self.current_block.rotate()
+		if self.block_inside() == False or self.block_fits() == False:
+			self.current_block.undo_rotation()
+		else:
+			self.rotate_sound.play()
+
+	# Chặn khối di chuyển ra ngoài bảng
+	def block_inside(self):
+		tiles = self.current_block.get_cell_positions()
+		for tile in tiles:
+			if self.grid.is_inside(tile.row, tile.column) == False:
+				return False
+		return True
+	
+	# Vẽ khối
+	def draw(self, screen):
+		self.grid.draw(screen)
+		self.current_block.draw(screen, 11, 11)
+
+		if self.next_block.id == 3:
+			self.next_block.draw(screen, 255, 290)
+		elif self.next_block.id == 4:
+			self.next_block.draw(screen, 255, 280)
+		else:
+			self.next_block.draw(screen, 270, 270)
